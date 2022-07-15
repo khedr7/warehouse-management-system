c9e1461996f27268286ec7b0b73b89e35fc21bfd
‪Khedr Muhieddin‬‏ <82679009+khedr7@users.noreply.github.com>
2022-07-15 18:17:36 +0300

    distributionCentercontroller CRUD

--- app/Http/Controllers/V1/DistributionCenterController.php
+++ app/Http/Controllers/V1/DistributionCenterController.php
@@ -15,7 +15,21 @@ class DistributionCenterController extends Controller
      */
     public function index()
     {
-        //
+        $distributionCenters = DistributionCenter::latest();
+
+        $data = [];
+        $i=0;
+        foreach ($distributionCenters as $distributionCenter) {
+            $data[$i] = [
+                'distributionCenter' => $distributionCenter,
+                'Media Items'        => $distributionCenter->getMedia('images')
+            ];
+            $i++;
+        }
+
+        return response()->json([
+            'Distribution Centers' => $data,
+        ], 200);
     }
 
     /**
@@ -26,7 +40,33 @@ class DistributionCenterController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validation = $request->validate([
+            'name'          => 'required|min:2|max:10',
+            'location_id'   => 'required|numeric|exists:locations,id',
+            'user_id'       => 'required|numeric|exists:users,id',
+            'images'        => 'required|array',
+            'images.*'      => 'required|file|image',
+        ]);
+
+        $distributionCenter = DistributionCenter::create($validation);
+
+        // add  images to distributionCenter using media library
+        if ($request->hasFile('images')) {
+            $fileAdders = $distributionCenter->addMultipleMediaFromRequest(['images'])
+            ->each(function ($fileAdder) {
+                $fileAdder->preservingOriginal()->toMediaCollection('images');
+            });
+        }
+
+        // checking the creation
+        if ($distributionCenter){
+            return response()->json([
+                'message' => "Distribution Center created successfully",
+            ], 201);
+        }
+        return response()->json([
+            'message' => 'Error',
+        ], 400);
     }
 
     /**
@@ -37,7 +77,15 @@ class DistributionCenterController extends Controller
      */
     public function show(DistributionCenter $distributionCenter)
     {
-        //
+        if ($distributionCenter){
+            return response()->json([
+                'Distribution Center' => $distributionCenter,
+                'Media Items'         => $distributionCenter->getMedia('images')
+            ], 200);
+        }
+        return response()->json([
+            'message' => 'Error',
+        ], 404);
     }
 
     /**
@@ -49,7 +97,37 @@ class DistributionCenterController extends Controller
      */
     public function update(Request $request, DistributionCenter $distributionCenter)
     {
-        //
+        $validation = $request->validate([
+            'name'          => 'required|min:2|max:10',
+            'location_id'   => 'required|numeric|exists:locations,id',
+            'user_id'       => 'required|numeric|exists:users,id',
+            'images'        => 'required|array',
+            'images.*'      => 'required|file|image',
+        ]);
+
+        $distributionCenter->name = $validation['name'];
+        $distributionCenter->location_id = $validation['location_id'];
+        $distributionCenter->user_id = $validation['user_id'];
+
+        // change the images (delete the previous collection and add new one)
+        if ($request->hasFile('images')) {
+            $distributionCenter->clearMediaCollection('images');
+            $fileAdders = $distributionCenter->addMultipleMediaFromRequest(['images'])
+            ->each(function ($fileAdder) {
+                $fileAdder->toMediaCollection('images');
+            });
+        }
+
+        $distributionCenter->save();
+
+        if ($distributionCenter){
+            return response()->json([
+                'message' => "Distribution Center edited successfully",
+            ], 200);
+        }
+        return response()->json([
+            'message' => 'Error',
+        ], 400);
     }
 
     /**
@@ -60,6 +138,16 @@ class DistributionCenterController extends Controller
      */
     public function destroy(DistributionCenter $distributionCenter)
     {
-        //
+        $distributionCenter->delete();
+
+        if($distributionCenter){
+            return response()->json([
+                'message' => 'Error',
+            ], 400);
+        }
+
+        return response()->json([
+            'message' => "Distribution Center deleted successfully",
+        ], 200);
     }
 }
