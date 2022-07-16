e72f6a3a49ac633b30ff1bd88008b74ac7634055
abd_alrahman <76664726+Abd-Alrahman-Alkateb@users.noreply.github.com>
2022-07-16 15:41:18 +0300

    install sanctum+ create AuthController+Auth route

---
+++ app/Http/Controllers/V1/AuthController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\V1;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * create user
+     */
+
+    public function register(Request $request)
+    {
+        $validation = $request->validate([
+            'fname'    => 'required',
+            'lname'    => 'required',
+            'email'    => 'required|email',
+            'password' => 'required',
+            'phone'    => 'required'
+        ]);
+
+        $validation['password'] = bcrypt($validation['password']);
+        $user = User::create($validation);
+        $token = $user->createToken('auth');
+            return [
+                'message' => 'User successfully registered!',
+                'data'    => [
+                    'name'  => $user->fname,
+                    'token' => $token->plainTextToken
+                ]
+            ];
+    }
+
+    /**
+     * login user
+     */
+
+    public function login (Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $user->tokens()->delete();
+            $token = $user->createToken('auth');
+            return [
+                'message' => 'login was successful',
+                'data'    => [
+                    'name'  => $user->fname,
+                    'token' => $token->plainTextToken
+                ]
+            ];
+        }
+
+        return [
+            'message' => 'email or password is wrong'
+        ];
+    }
+
+    /**
+     * logout user
+     */
+    public function logout (Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+        return [
+            'message' => 'You have successfully logged out and the token was successfully deleted'
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\V1\AuthController;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+Route::post('/logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
